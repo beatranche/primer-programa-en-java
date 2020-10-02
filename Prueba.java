703290ad1a21b5148732e24697730f8b106c3dd9
beatranche <56152901+beatranche@users.noreply.github.com>
2020-10-02 09:03:20 +0200

    Muestra residencia actual y sistema operativo

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,8 @@
 public class Prueba{
 	public static void main(String args[]){
 		System.out.println("Beatriz Tranche Robles");
-		System.out.println("Leon 1979");
+		System.out.println("Leon 1979");		
+		System.out.println("Leon Windows 10");
 	}
 	
 }
